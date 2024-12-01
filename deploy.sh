51f66b49358a8efb0b9c7c4696a3dabe9b42041d
BbqGamer <korba.adam@gmail.com>
2024-12-01 14:33:59 +0100

    Add deploy script

---
+++ deploy.sh
@@ -0,0 +1,17 @@
+# Script to deploy the hugo site to the server. It assumes that the server
+# is reachable via ssh configured in ~/.ssh/config with hostname $REMOTE_SSH_HOST
+# and that it has the webserver configured to serve content from the $REMOTE_DIR directory.
+
+if [ -z "$REMOTE_SSH_HOST" ]; then
+  echo "Please set REMOTE_SSH_HOST"
+  exit 1
+fi
+
+if [ -z "$REMOTE_DIR" ]; then
+  echo "Please set REMOTE_DIR"
+  exit 1
+fi
+
+hugo
+rsync -avz --delete public/ $REMOTE_SSH_HOST:$REMOTE_DIR
+
